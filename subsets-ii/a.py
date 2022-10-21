063d909cf1df2352e8e97ac981fb69ad3731cf41
masaki925 <masaki925@gmail.com>
2022-10-21 15:47:21 +0900

    add subsets-ii

---
+++ subsets-ii/a.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        sub = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(sub.copy())
+                return
+
+            sub.append(nums[i])
+            dfs(i + 1)
+            sub.pop()
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            dfs(i + 1)
+
+        dfs(0)
+
+        return res
+
+
+s = Solution()
+print(f"{s.subsetsWithDup([1,2,2])}")
