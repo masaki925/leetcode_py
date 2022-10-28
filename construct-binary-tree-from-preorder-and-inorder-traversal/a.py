0316dd414019fe050c85684692ed50a094212283
masaki925 <masaki925@gmail.com>
2022-10-28 12:40:55 +0900

    revert

--- construct-binary-tree-from-preorder-and-inorder-traversal/a.py
+++ construct-binary-tree-from-preorder-and-inorder-traversal/a.py
@@ -9,6 +9,25 @@ class TreeNode:
 
 
 class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
+
+        return root
+
+    def printTree(self, root):
+        if not root:
+            return
+
+        print(root.val)
+        self.printTree(root.left)
+        self.printTree(root.right)
+
 
 preo = [3, 9, 10, 20, 15, 7]
 ino = [9, 10, 3, 15, 20, 7]
@@ -18,4 +37,3 @@ sol = Solution()
 root = sol.buildTree(preo, ino)
 
 sol.printTree(root)
-
