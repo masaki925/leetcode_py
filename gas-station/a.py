59685f2df85d5a9de7c644b9444ee905b3d99423
masaki925 <masaki925@gmail.com>
2022-11-29 11:36:10 +0900

    add gas-station

---
+++ gas-station/a.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        res = 0
+        total = 0
+
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            if total < 0:
+                total = 0
+                res = i + 1
+
+        return res
+
+
+s = Solution()
+# print(s.canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
+
+print(s.canCompleteCircuit([3, 1, 1], [1, 2, 2]))
