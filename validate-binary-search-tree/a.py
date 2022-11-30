4793e1b06f9504c1973647f3bf684fe95a3bd889
masaki925 <masaki925@gmail.com>
2022-11-30 14:17:40 +0900

    refine validate-binary-search-tree

--- validate-binary-search-tree/a.py
+++ validate-binary-search-tree/a.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import List, Optional
 
-# Definition for a binary tree node.
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -9,27 +9,56 @@ class TreeNode:
 
 
 class Solution:
+    def __init__(self):
+        self.root = None
+
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node, minn, maxn):
+        def dfs(node, minN, maxN):
             if not node:
                 return True
 
-            return (
-                (minn < node.val)
-                and (node.val < maxn)
-                and dfs(node.left, minn, node.val)
-                and dfs(node.right, node.val, maxn)
-            )
+            # print(f"{node.val=}, {minN=}, {maxN=}")
+            if node.val <= minN:
+                return False
+            if maxN <= node.val:
+                return False
+            return dfs(node.left, minN, node.val) and dfs(node.right, node.val, maxN)
 
         return dfs(root, float("-inf"), float("inf"))
 
+    def buildTree(self, i, nums: List[int]) -> None:
+        n = len(nums)
+        if n == 0:
+            return None
 
-root = TreeNode(val=2, left=TreeNode(val=1), right=TreeNode(val=3))
-# root = TreeNode(
-#     val=5,
-#     left=TreeNode(val=4),
-#     right=TreeNode(val=6, left=TreeNode(val=3), right=TreeNode(val=7)),
-# )
+        def inner(i=0):
+            if n <= i or nums[i] is None:
+                return None
 
-sol = Solution()
-print(sol.isValidBST(root))
+            node = TreeNode(nums[i])
+            node.left = inner(2 * i + 1)
+            node.right = inner(2 * i + 2)
+            return node
+
+        return inner()
+
+    def showTree(self, node):
+        if not node:
+            return
+
+        print(f"{node.val=}")
+
+        if node.left:
+            self.showTree(node.left)
+        else:
+            print("None")
+        if node.right:
+            self.showTree(node.right)
+        else:
+            print("None")
+
+
+s = Solution()
+s.root = s.buildTree(0, [5, 1, 4, None, None, 3, 6])
+# s.showTree(s.root)
+print(s.isValidBST(s.root))
